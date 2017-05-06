7be8ddd965567d21aa127dda8b4bf792eb6505cf
harshbhardwaj29 <harshbhardwajcsit@gmail.com>
2017-05-06 14:48:02 +0530

    api response get

--- index.js
+++ index.js
@@ -33,9 +33,17 @@ app.post("/movetomainpage",function (req,res) {
     request(url, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        console.log(body); // Print the HTML for the Google homepage.
+
 
     });
+
+
+
+
+
+
+
 
     res.render('Result');
 
